11e34306189ddb6b3c024368f32206d335e92b27
Daviti <davit_saginashvili@yahoo.com>
2016-12-09 00:47:59 +0400

    remove tick

--- components/Examples.jsx
+++ components/Examples.jsx
@@ -4,7 +4,7 @@ class Examples extends React.Component {
     render() {
         return (
             <div>
-                <h2>Empty af for now!</h2>
+                <h2>Empty af for now</h2>
             </div>
         )
     }
